cecb0b174a5ea23f1370abdbfc65e4eefa5e9302
dmjohnson <dmjohnson@wlapcs.org>
2018-01-11 04:40:05 +0000

    built grayscale demo

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 function setup(){
     createCanvas(600,400);
+    background(256);
+    noStroke();
+    num = 0;
+    frameRate(10);
 }
 
 
 function draw(){
-    
+  if(num < 256){
+      fill(num);
+      ellipse(width/2, height/2, 250, 250);
+      
+      fill("#FF0000");
+      textAlign(CENTER, CENTER);
+      textSize(25);
+      text(num, width/2, height/2);
+      
+       num = num + 1;
+  }  
 }
